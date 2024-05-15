2aa3e96a3a6d855930e10748b0e65f59121665b6
morganhah99 <mjh58@njit.edu>
2024-05-15 18:47:45 -0400

    Formatted login page

--- app/src/main/java/com/example/valorantapi/LoginActivity.kt
+++ app/src/main/java/com/example/valorantapi/LoginActivity.kt
@@ -9,10 +9,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.credentials.CredentialManager
 import androidx.credentials.GetCredentialRequest
+import androidx.credentials.GetCredentialResponse
 import com.example.valorantapi.databinding.ActivityLoginBinding
 import com.example.valorantapi.ui.MainActivity
 import com.google.android.libraries.identity.googleid.GetGoogleIdOption
 import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential
+import com.google.android.libraries.identity.googleid.GoogleIdTokenParsingException
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -24,7 +26,8 @@ class LoginActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityLoginBinding
     lateinit var auth: FirebaseAuth
-
+    val WEB_CLIENT_ID =
+        "1086027571480-6d815j44iku92jl50vlo085nn0lq2oqe.apps.googleusercontent.com"
     private fun isVerifiedUser() {
         auth.signInWithEmailAndPassword(
             binding.etEmail.text.toString(),
@@ -66,45 +69,68 @@ class LoginActivity : AppCompatActivity() {
 
     }
 
+    private fun handleSignIn(result: GetCredentialResponse) {
+        val credential = result.credential
+
+        when (credential) {
+            is GoogleIdTokenCredential -> {
+                val googleIdToken = credential.idToken
+                Log.i(TAG, "Google ID Token: $googleIdToken")
+                Toast.makeText(this, "Google Sign In Successful", Toast.LENGTH_SHORT).show()
+            }
+            else -> {
+                Log.e(TAG, "Unexpected type of credential")
+                Toast.makeText(this, "Unexpected Credential Type", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
     private fun logInUsingGoogle() {
         val context = this
 
         val coroutineScope = CoroutineScope(Dispatchers.Main)
-        val WEB_CLIENT_ID = "1086027571480-6d815j44iku92jl50vlo085nn0lq2oqe.apps.googleusercontent.com"
+
         val credentialManager = CredentialManager.create(context)
         val rawNonce = UUID.randomUUID().toString()
         val bytes = rawNonce.toByteArray()
         val md = MessageDigest.getInstance("SHA-256")
         val digest = md.digest(bytes)
-        val hashedNonce = digest.fold(""){str, it -> str + "%02x".format(it) }
+        val hashedNonce = digest.fold("") { str, it -> str + "%02x".format(it) }
 
-        val googleIdOption: GetGoogleIdOption = GetGoogleIdOption
-            .Builder()
-            .setFilterByAuthorizedAccounts(false)
+        val googleIdOption: GetGoogleIdOption = GetGoogleIdOption.Builder()
+            .setFilterByAuthorizedAccounts(true)
             .setServerClientId(WEB_CLIENT_ID)
             .setNonce(hashedNonce)
             .build()
 
-        val request: GetCredentialRequest = GetCredentialRequest
-            .Builder()
+        val request: GetCredentialRequest = GetCredentialRequest.Builder()
             .addCredentialOption(googleIdOption)
             .build()
 
         coroutineScope.launch {
-            val result = credentialManager.getCredential(
-                request = request,
-                context = context,
-            )
-            val credential = result.credential
+            try {
+                val result = credentialManager.getCredential(
+                    request = request,
+                    context = context,
+                )
+                handleSignIn(result)
+                val credential = result.credential
 
-            val googleIdTokenCredential = GoogleIdTokenCredential
-                .createFrom(credential.data)
+                val googleIdTokenCredential = GoogleIdTokenCredential
+                    .createFrom(credential.data)
 
-            val googleIdToken = googleIdTokenCredential.idToken
+                val googleIdToken = googleIdTokenCredential.idToken
 
-            Log.i(TAG,googleIdToken )
+                Log.i(TAG, googleIdToken)
 
-            Toast.makeText(context, "Signed In", Toast.LENGTH_SHORT).show()
+                Toast.makeText(context, "Signed In", Toast.LENGTH_SHORT).show()
+            } catch (e: GoogleIdTokenParsingException) {
+                Toast.makeText(context, e.message, Toast.LENGTH_LONG).show()
+            }
+//            catch (e: androidx.credentials.exceptions.GetCredentialException) {
+//                Toast.makeText(context, e.message, Toast.LENGTH_LONG).show()
+//
+//            }
         }
 
     }
@@ -115,9 +141,10 @@ class LoginActivity : AppCompatActivity() {
         if (user != null) goToHomeActivity()
     }
 
-    fun goToHomeActivity() {
+    private fun goToHomeActivity() {
         startActivity(
-            Intent(this,
+            Intent(
+                this,
                 MainActivity::class.java
             )
         ).also { this@LoginActivity.finish() }
